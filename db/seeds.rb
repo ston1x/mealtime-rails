368172037d243bbc7b9405fd8d4a61f0266feedb
Nicolai Stoianov <stoianovnk@gmail.com>
2024-02-13 22:05:12 +0100

    Set up recipe seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+
+puts 'Reading recipes..'
+INGREDIENTS_SEPARATOR = ';'.freeze
+recipes = JSON.parse(File.read('db/seeds/recipes-en.json'))
+
+puts 'Seeding recipes..'
+recipes.each do |recipe|
+  Recipe.create!(
+    title: recipe['title'],
+    cook_time: recipe['cook_time'],
+    prep_time: recipe['prep_time'],
+    ingredients: recipe['ingredients'].map do |ingredient|
+      ingredient.gsub(/;/, ',') # Replace semicolons with commas in order to use semicolons as separator
+    end.join(INGREDIENTS_SEPARATOR),
+    ratings: recipe['ratings'],
+    category: recipe['category'].presence,
+    author: recipe['author'].presence,
+    image: recipe['image'].presence
+  )
+end
+puts 'Done!'
